06bf9e655cb66e44c1e1fcf8fe82e0e45f0bea65
andergcp <andergcp@hotmail.com>
2021-12-09 22:07:58 -0500

    Removing print of lists

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -7,7 +7,6 @@ def canUnlockAll(boxes):
             return False
         for j in i:
             newlist.append(j)
-    print(newlist)
     for index, keys in enumerate(boxes):
         if index in newlist or index < k-1:
             return True
